51f45d188b38efe8913675ef438cd9290b401d54
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-12-30 20:09:23 -0800

    Add ComposeVM and ComposeView files

---
+++ Tribes/View/Compose/ComposeVM.swift
@@ -0,0 +1,8 @@
+//
+//  ComposeVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2022-12-30.
+//
+
+import Foundation
---
+++ Tribes/View/Compose/ComposeView.swift
@@ -0,0 +1,20 @@
+//
+//  ComposeView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2022-12-30.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct ComposeView_Previews: PreviewProvider {
+	static var previews: some View {
+		ComposeView()
+	}
+}
