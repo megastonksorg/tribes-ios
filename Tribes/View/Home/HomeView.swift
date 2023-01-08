5d731922a4d59381b3b1bbd52a8901c1f884a88e
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-08 09:28:25 -0800

    Use the new PageView in the HomeView

--- Tribes/View/Home/HomeView.swift
+++ Tribes/View/Home/HomeView.swift
@@ -18,13 +18,10 @@ struct HomeView: View {
 	
 	var body: some View {
 		PageView(
-			selection:  $viewModel.currentPage,
-			content: {
-				[
-					HomeContentView(page: .compose, viewModel: viewModel),
-					HomeContentView(page: .tribes, viewModel: viewModel)
-				]
-			}
+			[
+				HomeContentView(page: .compose, viewModel: viewModel),
+				HomeContentView(page: .tribes, viewModel: viewModel)
+			]
 		)
 		.ignoresSafeArea()
 	}
@@ -33,17 +30,5 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
 	static var previews: some View {
 		HomeView(viewModel: .init())
-	}
-}
-
-extension PageView {
-	init(selection: Binding<HomeView.ViewModel.Page>, content: @escaping () -> [Content]) {
-		self.init(
-			selection: Binding<Int>(
-				get: { HomeView.ViewModel.Page.allCases.firstIndex(of: selection.wrappedValue)! },
-				set: { selection.wrappedValue = HomeView.ViewModel.Page.allCases[$0] }
-			),
-			content: content
-		)
 	}
 }
