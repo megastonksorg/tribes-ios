29f5dc95e24bc2776153369e0a90a15b606e126b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-09 13:50:16 -0800

    Make TribeMember conform to identifiable

--- Tribes/Model/TribeMember.swift
+++ Tribes/Model/TribeMember.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 
-struct TribeMember: Encodable {
+struct TribeMember: Encodable, Identifiable {
+	let id: String
 	let name: String
 	let photoURL: URL
 }
 
 extension TribeMember {
 	static let noop: TribeMember = TribeMember(
+		id: "1",
 		name: "Kingsley Okeke",
 		photoURL: URL(string: "https://megastonksfilestoragedev.blob.core.windows.net/images/001d0c1e-a971-47cd-ad53-eb468e4d3d94.png")!
 	)
