261c4e672b3e27d6c286be0d3635adba36ca33ae
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-09 15:06:22 -0800

    Add Avatar view for one person

--- Tribes/Utilities/Views/TribeAvatar.swift
+++ Tribes/Utilities/Views/TribeAvatar.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 struct TribeAvatar: View {
 	let members: [TribeMember]
+	let size: CGFloat
 	
 	var body: some View {
 		Circle()
+			.fill(Color.app.primary)
+			.frame(dimension: size)
+			.overlay {
+				switch members.count {
+				case 1:
+					asyncImage(user: members[0])
+						.frame(dimension: size * 0.8)
+				case 2:
+					Circle()
+				default:
+					Circle()
+				}
+			}
+	}
+	
+	@ViewBuilder
+	func asyncImage(user: TribeMember) -> some View {
+		AsyncImage(url: user.photoURL,
+		content: { image in
+			image
+				.resizable()
+				.scaledToFill()
+				.clipShape(Circle())
+		}, placeholder: {
+			Circle()
+				.fill(Color.black.opacity(0.6))
+				.overlay(ProgressView())
+		})
 	}
 }
 
@@ -19,14 +48,10 @@ struct TribeAvatar_Previews: PreviewProvider {
 	static var previews: some View {
 		TribeAvatar(
 			members: Array(
-				repeating: TribeMember(
-					id: UUID().uuidString,
-					name: "Kingsley Okeke",
-					photoURL: URL(string: "https://megastonksfilestoragedev.blob.core.windows.net/images/001d0c1e-a971-47cd-ad53-eb468e4d3d94.png")!
-				),
-				count: 10
-			)
+				repeating: TribeMember.noop,
+				count: 1
+			),
+			size: 200
 		)
-		.frame(dimension: 200)
 	}
 }
