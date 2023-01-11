87b19b2582354eb9d062bc09cc7b58867b899dc7
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-10 18:37:48 -0800

    Add the Avatar view for 7 tribe members

--- Tribes/Utilities/Views/TribeAvatar.swift
+++ Tribes/Utilities/Views/TribeAvatar.swift
@@ -134,6 +134,39 @@ struct TribeAvatar: View {
 						}
 					}
 					.frame(dimension: stackSize)
+				case 7:
+					let size1: CGFloat = stackSize * 0.38
+					let size2: CGFloat = stackSize * 0.36
+					let size3: CGFloat = stackSize * 0.32
+					HStack(spacing: 0) {
+						VStack {
+							asyncImage(user: members[0])
+								.frame(dimension: size2)
+							asyncImage(user: members[1])
+								.frame(dimension: size2)
+						}
+
+						VStack {
+							asyncImage(user: members[2])
+								.frame(dimension: size2)
+								.offset(x: size2 * 0.2, y: size2 * 0.1)
+							asyncImage(user: members[3])
+								.frame(dimension: size2)
+							asyncImage(user: members[4])
+								.frame(dimension: size3)
+								.offset(x: -size3 * 0.08)
+						}
+						
+						VStack {
+							asyncImage(user: members[5])
+								.frame(dimension: size2)
+								.offset(x: size2 * 0.1, y: size2 * 0.1)
+							asyncImage(user: members[6])
+								.frame(dimension: size1)
+						}
+						.offset(y: size1 * 0.16)
+					}
+					.frame(dimension: stackSize)
 				default:
 					Circle()
 				}
@@ -161,7 +194,7 @@ struct TribeAvatar_Previews: PreviewProvider {
 		TribeAvatar(
 			members: Array(
 				repeating: TribeMember.noop,
-				count: 6
+				count: 7
 			),
 			size: 200
 		)
