d8e79a74af49ab7694c1ea7f246225d191caa955
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-12 13:04:18 -0800

    Add Custom Font Kreon-Bold Add TextView and some Text Styles

---
+++ Tribes/Utilities/Styles/Fonts.swift
@@ -0,0 +1,12 @@
+//
+//  Fonts.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-12.
+//
+
+import Foundation
+
+struct FontNames {
+	static let kreon: String = "Kreon-Bold"
+}
---
+++ Tribes/Utilities/Views/TextView.swift
@@ -0,0 +1,60 @@
+//
+//  TextView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-12.
+//
+
+import SwiftUI
+
+struct TextViewModifier: ViewModifier {
+	enum Style {
+		case appTitle
+		case tribeName(_ size: CGFloat)
+	}
+	
+	let style: Style
+	
+	func body(content: Content) -> some View {
+		switch style {
+		case .appTitle:
+			content
+				.font(.custom(FontNames.kreon, fixedSize: 40))
+				.foregroundColor(Color.app.tertiary)
+		case .tribeName(let size):
+			content
+				.font(.system(size: size, weight: .semibold, design: .rounded))
+				.foregroundColor(Color.app.tertiary)
+		}
+	}
+}
+
+fileprivate extension View {
+	func styleText(style: TextViewModifier.Style) -> some View {
+		modifier(TextViewModifier(style: style))
+	}
+}
+
+struct TextView: View {
+	let content: String
+	let style: TextViewModifier.Style
+	
+	init(_ content: String, style: TextViewModifier.Style) {
+		self.content = content
+		self.style = style
+	}
+	
+	var body: some View {
+		Text(content)
+			.styleText(style: style)
+	}
+}
+
+struct TextView_Previews: PreviewProvider {
+	static var previews: some View {
+		VStack {
+			TextView("Tribes", style: .appTitle)
+			TextView("Belly Squad", style: .tribeName(20))
+		}
+	}
+}
