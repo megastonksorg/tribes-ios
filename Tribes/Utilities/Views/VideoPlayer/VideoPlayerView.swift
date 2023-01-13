f2643c9b0e97c0e260bc8a095b21679b4d12926a
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-12 21:24:08 -0800

    Fix bug where the player would continue playing even when it is not visible in the screen

--- Tribes/Utilities/Views/VideoPlayer/VideoPlayerView.swift
+++ Tribes/Utilities/Views/VideoPlayer/VideoPlayerView.swift
@@ -9,27 +9,28 @@ import AVFoundation
 import SwiftUI
 
 struct VideoPlayerView: View {
-	let url: URL
 	let thumbnail: UIImage
+	let url: URL
 	
+	@State private var isPlaying = false
 	@State private var shouldShowThumbnail = true
 	@State private var playbackProgress = Float(0)
 	
 	init(url: URL) {
-		self.url = url
 		self.thumbnail = {
 			let imageGenerator = AVAssetImageGenerator(asset: AVAsset(url: url))
 			imageGenerator.appliesPreferredTrackTransform = true
 			let cgImage = try? imageGenerator.copyCGImage(at: CMTime(seconds: 0, preferredTimescale: 1), actualTime: nil)
 			return cgImage.map(UIImage.init) ?? .init()
 		}()
+		self.url = url
 	}
 	
 	var body: some View {
 		ZStack {
 			PlayerView(
 				url: url,
-				isPlaying: true,
+				isPlaying: isPlaying,
 				isMuted: false,
 				shouldShowThumbnail: $shouldShowThumbnail,
 				onPlaybackProgressChange: { progress in playbackProgress = progress }
@@ -43,6 +44,22 @@ struct VideoPlayerView: View {
 			Color.clear
 				.preference(key: PlaybackProgressKey.self, value: playbackProgress)
 		}
+		.background {
+			GeometryReader { proxy in
+				Color.clear
+					.preference(
+						key: PlayerVisibilityKey.self,
+						value: proxy.frame(in: .global).maxX > UIScreen.main.bounds.maxX * 0.8
+					)
+					.onPreferenceChange(PlayerVisibilityKey.self) { isVisible in
+						if isVisible {
+							self.isPlaying = true
+						} else {
+							self.isPlaying = false
+						}
+					}
+			}
+		}
 	}
 }
 
@@ -54,7 +71,12 @@ struct VideoPlayerView_Previews: PreviewProvider {
 	}
 }
 
-struct PlaybackProgressKey: PreferenceKey {
+fileprivate struct PlayerVisibilityKey: PreferenceKey {
+	static var defaultValue: Bool = false
+	static func reduce(value: inout Bool, nextValue: () -> Bool) { }
+}
+
+fileprivate struct PlaybackProgressKey: PreferenceKey {
 	static var defaultValue: Float = 0
 	static func reduce(value: inout Float, nextValue: () -> Float) { value = nextValue() }
 }
