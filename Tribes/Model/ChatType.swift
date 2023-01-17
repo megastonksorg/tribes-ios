dfff05b7c2462715543d82e98f7e675b21696220
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-16 16:30:42 -0800

    Add Chat type model and style the content of the ChatBubble accordingly

---
+++ Tribes/Model/ChatType.swift
@@ -0,0 +1,13 @@
+//
+//  ChatType.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-16.
+//
+
+import Foundation
+
+enum ChatType {
+	case incoming
+	case outgoing
+}
--- Tribes/Utilities/Views/ChatBubble.swift
+++ Tribes/Utilities/Views/ChatBubble.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct ChatBubble: View {
+	let cornerRadius: CGFloat = 30
+	
+	let content: String
+	let type: ChatType
+	
 	var body: some View {
-		Text("Hello, World!")
+		let corners: UIRectCorner = {
+			switch type {
+			case .incoming: return [.topRight, .bottomLeft, .bottomRight]
+			case .outgoing: return [.topLeft, .topRight, .bottomLeft]
+			}
+		}()
+		
+		let color: Color = {
+			switch type {
+			case .incoming: return Color.app.secondary
+			case .outgoing: return Color.app.tertiary
+			}
+		}()
+		
+		let foregroundColor: Color = {
+			switch type {
+			case .incoming: return Color.white
+			case .outgoing: return Color.black
+			}
+		}()
+		
+		Text(content)
+			.font(Font.app.subTitle)
+			.foregroundColor(foregroundColor)
+			.padding()
+			.background(
+				CustomRoundedRectangle(cornerRadius: cornerRadius, corners: corners)
+					.fill(color)
+			)
 	}
 }
 
 struct ChatBubble_Previews: PreviewProvider {
 	static var previews: some View {
-		ChatBubble()
+		VStack {
+			ChatBubble(content: "Stop there. Please change account info when you get a chance", type: .incoming)
+		}
+		.pushOutFrame()
+		.background(Color.app.background)
 	}
 }
