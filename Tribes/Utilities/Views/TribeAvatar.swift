6c0d681fc611e3d842386ce71fbb971fabaa3676
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-18 17:50:03 -0800

    Add UserAvatar to TribesAvatar

--- Tribes/Utilities/Views/TribeAvatar.swift
+++ Tribes/Utilities/Views/TribeAvatar.swift
@@ -39,7 +39,7 @@ struct TribeAvatar: View {
 				.overlay {
 					switch members.count {
 					case 1:
-						asyncImage(user: members[0])
+						userAvatar(user: members[0])
 							.frame(dimension: maxSize)
 					case 2:
 						let size1: CGFloat = stackSize * 0.4
@@ -47,11 +47,11 @@ struct TribeAvatar: View {
 						VStack(alignment: .leading, spacing: 0) {
 							HStack {
 								Spacer()
-								asyncImage(user: members[0])
+								userAvatar(user: members[0])
 									.frame(dimension: size1)
 									.offset(y: size1 * 0.2)
 							}
-							asyncImage(user: members[1])
+							userAvatar(user: members[1])
 								.frame(dimension: size2)
 						}
 						.frame(dimension: stackSize)
@@ -63,13 +63,13 @@ struct TribeAvatar: View {
 							Spacer()
 							Spacer()
 							HStack {
-								asyncImage(user: members[0])
+								userAvatar(user: members[0])
 									.frame(dimension: size1)
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
 									.offset(y: size2 * 0.2)
 							}
-							asyncImage(user: members[2])
+							userAvatar(user: members[2])
 								.frame(dimension: size3)
 						}
 						.frame(dimension: stackSize)
@@ -80,16 +80,16 @@ struct TribeAvatar: View {
 						let size4: CGFloat = stackSize * 0.43
 						VStack(spacing: 0) {
 							HStack {
-								asyncImage(user: members[0])
+								userAvatar(user: members[0])
 									.frame(dimension: size1)
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
 							}
 							Spacer()
 							HStack {
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size3)
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size4)
 									.offset(y: -size4 * 0.2)
 							}
@@ -103,20 +103,20 @@ struct TribeAvatar: View {
 						let size5: CGFloat = stackSize * 0.30
 						VStack(spacing: 0) {
 							HStack {
-								asyncImage(user: members[0])
+								userAvatar(user: members[0])
 									.frame(dimension: size1)
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
 							}
 							Spacer()
 							HStack {
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size3)
 									.offset(x: size3 * 0.2, y: -size3 * 0.2)
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size4)
 									.offset(x: size4 * 0.05, y: size4 * 0.2)
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size5)
 									.offset(x: -size5 * 0.2, y: -size5 * 0.5)
 							}
@@ -128,21 +128,21 @@ struct TribeAvatar: View {
 						let size3: CGFloat = stackSize * 0.32
 						let size4: CGFloat = stackSize * 0.28
 						VStack(spacing: 0) {
-							asyncImage(user: members[0])
+							userAvatar(user: members[0])
 								.frame(dimension: size2)
 							HStack {
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size4)
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size1)
 									.offset(y: -size1 * 0.2)
 							}
 							HStack {
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size2)
-								asyncImage(user: members[5])
+								userAvatar(user: members[5])
 									.frame(dimension: size3)
 							}
 						}
@@ -153,28 +153,28 @@ struct TribeAvatar: View {
 						let size3: CGFloat = stackSize * 0.32
 						HStack(spacing: 0) {
 							VStack {
-								asyncImage(user: members[0])
+								userAvatar(user: members[0])
 									.frame(dimension: size2)
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
 							}
 
 							VStack {
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size2)
 									.offset(x: size2 * 0.2, y: size2 * 0.1)
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size2)
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size3)
 									.offset(x: -size3 * 0.08)
 							}
 							
 							VStack {
-								asyncImage(user: members[5])
+								userAvatar(user: members[5])
 									.frame(dimension: size2)
 									.offset(x: size2 * 0.1, y: size2 * 0.1)
-								asyncImage(user: members[6])
+								userAvatar(user: members[6])
 									.frame(dimension: size1)
 							}
 							.offset(y: size1 * 0.16)
@@ -186,38 +186,38 @@ struct TribeAvatar: View {
 						let size3: CGFloat = stackSize * 0.32
 						let size4: CGFloat = stackSize * 0.26
 						HStack(spacing: 0) {
-							asyncImage(user: members[0])
+							userAvatar(user: members[0])
 								.frame(dimension: size4)
 								.offset(x: size4 * 0.2)
 							VStack {
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2, y: size2 * 0.1)
 								Spacer()
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2)
 							}
 							
 							VStack {
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size2)
 									.offset(x: size2 * 0.05)
 								Spacer()
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2)
 								Spacer()
-								asyncImage(user: members[5])
+								userAvatar(user: members[5])
 									.frame(dimension: size3)
 							}
 							.offset(x: -size2 * 0.2)
 							
 							VStack(spacing: size2 * 0.2) {
-								asyncImage(user: members[6])
+								userAvatar(user: members[6])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2, y: size2 * 0.1)
-								asyncImage(user: members[7])
+								userAvatar(user: members[7])
 									.frame(dimension: size1)
 									.offset(x: -size2 * 0.2)
 							}
@@ -229,40 +229,40 @@ struct TribeAvatar: View {
 						let size2: CGFloat = stackSize * 0.32
 						let size3: CGFloat = stackSize * 0.26
 						HStack(spacing: 0) {
-							asyncImage(user: members[0])
+							userAvatar(user: members[0])
 								.frame(dimension: size3)
 								.offset(x: size3 * 0.2)
 							VStack {
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size1)
 									.offset(x: -size1 * 0.2, y: size1 * 0.1)
 								Spacer()
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size1)
 									.offset(x: -size1 * 0.2)
 							}
 							
 							VStack {
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size1)
 								Spacer()
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size1)
 									.offset(x: -size1 * 0.3)
 								Spacer()
-								asyncImage(user: members[5])
+								userAvatar(user: members[5])
 									.frame(dimension: size2)
 							}
 							.offset(x: -size1 * 0.2)
 							
 							VStack(spacing: size3 * 0.2) {
-								asyncImage(user: members[6])
+								userAvatar(user: members[6])
 									.frame(dimension: size3)
 									.offset(x: -size3 * 0.2, y: size3 * 0.1)
-								asyncImage(user: members[7])
+								userAvatar(user: members[7])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2)
-								asyncImage(user: members[8])
+								userAvatar(user: members[8])
 									.frame(dimension: size3)
 									.offset(x: -size3 * 0.2, y: -size3 * 0.1)
 							}
@@ -275,21 +275,21 @@ struct TribeAvatar: View {
 						let size2: CGFloat = stackSize * 0.32
 						let size3: CGFloat = stackSize * 0.26
 						HStack(spacing: 0) {
-							asyncImage(user: members[0])
+							userAvatar(user: members[0])
 								.frame(dimension: size3)
 								.offset(x: size3 * 0.2)
 							VStack {
-								asyncImage(user: members[1])
+								userAvatar(user: members[1])
 									.frame(dimension: size1)
 									.offset(x: -size1 * 0.2, y: size1 * 0.1)
 								Spacer()
-								asyncImage(user: members[2])
+								userAvatar(user: members[2])
 									.frame(dimension: size1)
 									.offset(x: -size1 * 0.2)
 							}
 							
 							VStack {
-								asyncImage(user: members[3])
+								userAvatar(user: members[3])
 									.frame(dimension: size1)
 								Spacer()
 								Circle()
@@ -302,19 +302,19 @@ struct TribeAvatar: View {
 									)
 									.offset(x: -size1 * 0.2)
 								Spacer()
-								asyncImage(user: members[4])
+								userAvatar(user: members[4])
 									.frame(dimension: size2)
 							}
 							.offset(x: -size1 * 0.2)
 							
 							VStack(spacing: size3 * 0.2) {
-								asyncImage(user: members[5])
+								userAvatar(user: members[5])
 									.frame(dimension: size3)
 									.offset(x: -size3 * 0.2, y: size3 * 0.1)
-								asyncImage(user: members[6])
+								userAvatar(user: members[6])
 									.frame(dimension: size2)
 									.offset(x: -size2 * 0.2)
-								asyncImage(user: members[7])
+								userAvatar(user: members[7])
 									.frame(dimension: size3)
 									.offset(x: -size3 * 0.4, y: -size3 * 0.1)
 							}
@@ -328,21 +328,8 @@ struct TribeAvatar: View {
 	}
 	
 	@ViewBuilder
-	func asyncImage(user: TribeMember) -> some View {
-		AsyncImage(url: user.photoURL,
-		content: { image in
-			image
-				.resizable()
-				.scaledToFill()
-				.clipShape(Circle())
-		}, placeholder: {
-			Circle()
-				.fill(Color.black.opacity(0.6))
-				.overlay(
-					ProgressView()
-						.controlSize(.mini)
-				)
-		})
+	func userAvatar(user: TribeMember) -> some View {
+		UserAvatar(url: user.photoURL)
 	}
 }
 
---
+++ Tribes/Utilities/Views/UserAvatar.swift
@@ -0,0 +1,34 @@
+//
+//  UserAvatar.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-18.
+//
+
+import SwiftUI
+
+struct UserAvatar: View {
+	let url: URL
+	var body: some View {
+		AsyncImage(url: url,
+		content: { image in
+			image
+				.resizable()
+				.scaledToFill()
+				.clipShape(Circle())
+		}, placeholder: {
+			Circle()
+				.fill(Color.black.opacity(0.6))
+				.overlay(
+					ProgressView()
+						.controlSize(.mini)
+				)
+		})
+	}
+}
+
+struct UserAvatar_Previews: PreviewProvider {
+	static var previews: some View {
+		UserAvatar(url: User.noop.profilePhoto)
+	}
+}
