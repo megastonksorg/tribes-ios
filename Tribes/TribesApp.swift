cd4df8d07d83fc5d13f8d6de5a5c0bdc87bd41b3
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-22 17:05:02 -0800

    Override the app window user interface style because context menu does not respect preferredColourScheme

--- Tribes/TribesApp.swift
+++ Tribes/TribesApp.swift
@@ -12,6 +12,11 @@ struct TribesApp: App {
 	var body: some Scene {
 		WindowGroup {
 			AppView()
+				.onAppear {
+					if let keyWindow = UIApplication.shared.connectedScenes.compactMap({ $0 as? UIWindowScene }).flatMap({ $0.windows }).first(where: { $0.isKeyWindow }) {
+						keyWindow.overrideUserInterfaceStyle = .dark
+					}
+				}
 		}
 	}
 }
