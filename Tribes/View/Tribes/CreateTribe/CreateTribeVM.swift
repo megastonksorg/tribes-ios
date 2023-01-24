43888f0453cf3c8b9cd7744d61319e5842465bdf
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-23 23:50:41 -0800

    Add files for CreateTribeView and CreateTribeVM

---
+++ Tribes/View/Tribes/CreateTribe/CreateTribeVM.swift
@@ -0,0 +1,14 @@
+//
+//  CreateTribeVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-23.
+//
+
+import Foundation
+
+extension CreateTribeView {
+	@MainActor class ViewModel: ObservableObject {
+		
+	}
+}
---
+++ Tribes/View/Tribes/CreateTribe/CreateTribeView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTribeVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-01-23.
+//
+
+import SwiftUI
+
+struct CreateTribeView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct CreateTribeView_Previews: PreviewProvider {
+	static var previews: some View {
+		CreateTribeView()
+	}
+}
