803b18315d5241292b05ca60480df7528a09b9df
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-24 02:02:46 -0800

    Remove the leading position from the AppToolBar then use the TextView for it's principal title

--- Tribes/Utilities/Views/AppToolBar.swift
+++ Tribes/Utilities/Views/AppToolBar.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct AppToolBar: View {
 	enum Position {
-		case leading
 		case principal
 		case trailing
 	}
@@ -18,32 +17,24 @@ struct AppToolBar: View {
 	let principalTitle: String
 	let trailingTitle: String
 	
-	let leadingClosure: () -> ()
 	let trailingClosure: () -> ()
 	
 	init(_ position: Position,
 		 principalTitle: String = "",
 		 trailingTitle: String = "Done",
-		 leadingClosure: @escaping () -> () = {},
 		 trailingClosure: @escaping () -> () = {}
 	) {
 		self.position = position
 		self.principalTitle = principalTitle
 		self.trailingTitle = trailingTitle
-		self.leadingClosure = leadingClosure
 		self.trailingClosure = trailingClosure
 	}
 	
 	var body: some View {
 		Group {
 			switch position {
-			case .leading:
-				Button(action: { self.leadingClosure() }) {
-					Image(systemName: "arrow.left")
-				}
 			case .principal:
-				Text(self.principalTitle)
-					.fontWeight(.bold)
+				TextView(principalTitle, style: .pageTitle)
 			case .trailing:
 				Button(action: { self.trailingClosure() }) {
 					Text(self.trailingTitle)
