9656c1c4e93c0bb0ac99c5100d9a31be9175edc5
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-02 15:30:00 -0800

    Add the Leave group with the LeaveTribeView and LeaveTribeVM files

---
+++ Tribes/View/Tribes/Leave/LeaveTribeVM.swift
@@ -0,0 +1,8 @@
+//
+//  LeaveTribeVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-02.
+//
+
+import Foundation
---
+++ Tribes/View/Tribes/Leave/LeaveTribeView.swift
@@ -0,0 +1,20 @@
+//
+//  LeaveTribeView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-02.
+//
+
+import SwiftUI
+
+struct LeaveTribeView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct LeaveTribeView_Previews: PreviewProvider {
+	static var previews: some View {
+		LeaveTribeView()
+	}
+}
