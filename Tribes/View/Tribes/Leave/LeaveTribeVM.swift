32f2fda5e9f0013df324d0d9b4c054101da51269
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-02 16:06:32 -0800

    Add the page title for LeaveTribeView

--- Tribes/View/Tribes/Leave/LeaveTribeVM.swift
+++ Tribes/View/Tribes/Leave/LeaveTribeVM.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension LeaveTribeView {
+	@MainActor class ViewModel: ObservableObject {
+		enum FocusField: Hashable {
+		  case field
+		}
+		
+		let tribe: Tribe
+		
+		@Published var isLoading: Bool = false
+		@Published var banner: BannerData?
+		
+		init(tribe: Tribe) {
+			self.tribe = tribe
+		}
+	}
+}
--- Tribes/View/Tribes/Leave/LeaveTribeView.swift
+++ Tribes/View/Tribes/Leave/LeaveTribeView.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct LeaveTribeView: View {
+	@FocusState private var focusedField: ViewModel.FocusField?
+	
+	@StateObject var viewModel: ViewModel
+	
+	init(viewModel: ViewModel) {
+		self._viewModel = StateObject(wrappedValue: viewModel)
+	}
+	
 	var body: some View {
-		Text("Hello, World!")
+		VStack(spacing: 20) {
+			SymmetricHStack(
+				content: {
+					Text("Leave?")
+				},
+				leading: { EmptyView() },
+				trailing: {
+					Button(action: {}) {
+						Image(systemName: "xmark")
+					}
+				}
+			)
+			.font(Font.app.title2)
+			.fontWeight(.semibold)
+			.foregroundColor(Color.app.tertiary)
+			.padding(.horizontal)
+			
+			VStack(spacing: 20) {
+				Text("Are you sure you want to leave ‘\(viewModel.tribe.name)’ tribe?")
+				Text("If you are sure, please type")
+				+
+				Text(" Leave ")
+					.foregroundColor(Color.app.tertiary)
+					.bold()
+				+
+				Text("below")
+			}
+			.multilineTextAlignment(.center)
+			.font(Font.app.title2)
+			.foregroundColor(.white)
+			.padding(.top)
+			
+			Spacer()
+		}
+		.pushOutFrame()
+		.overlay(isShown: viewModel.isLoading) {
+			AppProgressView()
+		}
+		.banner(data: self.$viewModel.banner)
+		.background(Color.app.background)
 	}
 }
 
 struct LeaveTribeView_Previews: PreviewProvider {
 	static var previews: some View {
-		LeaveTribeView()
+		LeaveTribeView(viewModel: .init(tribe: Tribe.noop))
 	}
 }
