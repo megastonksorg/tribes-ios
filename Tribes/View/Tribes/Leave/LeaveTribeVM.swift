fd6d751d2cdbf0bf260605987de6678eafb792be
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-02 17:51:18 -0800

    Add api call to leave a tribe

--- Tribes/View/Tribes/Leave/LeaveTribeVM.swift
+++ Tribes/View/Tribes/Leave/LeaveTribeVM.swift
@@ -5,6 +5,7 @@
 //  Created by Kingsley Okeke on 2023-02-02.
 //
 
+import Combine
 import Foundation
 
 extension LeaveTribeView {
@@ -17,13 +18,19 @@ extension LeaveTribeView {
 		let tribe: Tribe
 		let tribeMembers: [TribeMember]
 		
+		var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+		
 		var isConfirmed: Bool {
 			confirmation == ViewModel.confirmationTitle
 		}
 		
 		@Published var confirmation: String = ""
+		@Published var didLeaveTribe: Bool = false
 		@Published var isLoading: Bool = false
 		@Published var banner: BannerData?
+		
+		//Clients
+		let apiClient: APIClient = APIClient.shared
 		
 		init(tribe: Tribe) {
 			self.tribeMembers = {
@@ -33,6 +40,27 @@ extension LeaveTribeView {
 				return tribe.members
 			}()
 			self.tribe = tribe
+		}
+		
+		func leaveTribe() {
+			self.isLoading = true
+			self.apiClient
+				.leaveTribe(tribeID: tribe.id)
+				.receive(on: DispatchQueue.main)
+				.sink(
+					receiveCompletion: { [weak self] completion in
+						switch completion {
+						case .finished: return
+						case .failure(let error):
+							self?.isLoading = false
+							self?.banner = BannerData(error: error)
+						}
+					}, receiveValue: { [weak self] successResponse in
+						self?.isLoading = false
+						self?.didLeaveTribe = successResponse.success
+					}
+				)
+				.store(in: &self.cancellables)
 		}
 	}
 }
