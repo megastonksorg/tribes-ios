d18ece96438a2e8066765c61c45af2159d357a8a
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-03 15:39:02 -0800

    Add the group and file for the CacheClient

---
+++ Tribes/Clients/CacheClient/CacheClient.swift
@@ -0,0 +1,16 @@
+//
+//  CacheClient.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-03.
+//
+
+import Foundation
+
+protocol CacheClientProtocol {
+	
+}
+
+class CacheClient: CacheClientProtocol {
+	
+}
