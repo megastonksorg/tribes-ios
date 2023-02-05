37034b9b95e336c3f4d5398210cd29ce916cba3c
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-05 11:19:16 -0800

    Add file for app context menu to come

---
+++ Tribes/Utilities/Views/AppContextMenu.swift
@@ -0,0 +1,20 @@
+//
+//  AppContextMenu.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-05.
+//
+
+import SwiftUI
+
+struct AppContextMenu: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct AppContextMenu_Previews: PreviewProvider {
+	static var previews: some View {
+		AppContextMenu()
+	}
+}
