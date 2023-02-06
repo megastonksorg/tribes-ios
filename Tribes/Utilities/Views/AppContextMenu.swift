13af9c01bd74bcd38ce6a7700f9b3f93b6c5d426
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-06 12:05:04 -0800

    Add BoundsPreference key

--- Tribes/Utilities/Views/AppContextMenu.swift
+++ Tribes/Utilities/Views/AppContextMenu.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+
+struct BoundsPreference: PreferenceKey {
+	static var defaultValue: [String: Anchor<CGRect>] = [:]
+	
+	static func reduce(value: inout [String : Anchor<CGRect>], nextValue: () -> [String : Anchor<CGRect>]) {
+		value.merge(nextValue()){$1}
+	}
+}
+
 struct AppContextMenu: View {
 	var body: some View {
 		Text("Hello, World!")
