ace1735f3602b44d8ab1f397662785b88a918d94
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-10 12:06:40 -0800

    Add header and footer base view with text to the OnBoardingPageView

--- Tribes/View/Welcome/Onboarding/OnBoardingPageView.swift
+++ Tribes/View/Welcome/Onboarding/OnBoardingPageView.swift
@@ -14,14 +14,70 @@ struct OnBoardingPageView: View {
 		case sendInvites
 		case shareTea
 		case sendMessages
+		
+		var header: String {
+			switch self {
+			case .stayConnected: return ""
+			case .createTribe: return "Step 1"
+			case .sendInvites: return "Step 2"
+			case .shareTea: return "Step 3"
+			case .sendMessages: return "Step 4"
+			}
+		}
+		
+		var footer: String {
+			switch self {
+			case .stayConnected: return "Stay connected with the people who matter the most to you"
+			case .createTribe: return "Create a Tribe"
+			case .sendInvites: return "Send secure invites to your tribe members"
+			case .shareTea: return "Share Tea through the day with your tribe members"
+			case .sendMessages: return "Exchange secure messages with your tribe members"
+			}
+		}
 	}
+	
+	let page: Page
+	
 	var body: some View {
-		Text("Hello, World!")
+		VStack {
+			header(page: page)
+			body(page: page)
+			footer(page: page)
+		}
+	}
+	
+	@ViewBuilder
+	func textView(text: String) -> some View {
+		Text(text)
+			.font(.system(size: FontSizes.title2, weight: .semibold, design: .rounded))
+			.foregroundColor(.white)
+			.multilineTextAlignment(.center)
+	}
+	
+	@ViewBuilder
+	func header(page: Page) -> some View {
+		textView(text: page.header)
+	}
+	
+	@ViewBuilder
+	func body(page: Page) -> some View {
+		switch page {
+		case .stayConnected:
+			Color.black
+		default:
+			EmptyView()
+		}
+	}
+	
+	@ViewBuilder
+	func footer(page: Page) -> some View {
+		textView(text: page.footer)
 	}
 }
 
 struct OnBoardingPageView_Previews: PreviewProvider {
 	static var previews: some View {
-		OnBoardingPageView()
+		OnBoardingPageView(page: .stayConnected)
+			.preferredColorScheme(.dark)
 	}
 }
