6443825a10527662da0a597dea3c774454712b54
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-11 07:59:36 -0800

    Update the CalloutView to accept text content

--- Tribes/Utilities/Views/CalloutView.swift
+++ Tribes/Utilities/Views/CalloutView.swift
@@ -8,24 +8,38 @@
 import SwiftUI
 
 struct CalloutView: View {
-	let size: CGSize
-	let fill: Color
+	let content: String
+	let fill: Color = Color.app.secondary
+	
+	@State var width: CGFloat = .zero
+	
 	var body: some View {
-		VStack(spacing: 0) {
-			Capsule()
-				.fill(fill)
-				.frame(width: size.width, height: size.height)
-			RoundedTriangle(radius: 4)
-				.fill(fill)
-				.frame(dimension: 26)
-				.rotationEffect(.degrees(90))
-				.offset(y: -8)
-		}
+		Text(content)
+			.font(Font.app.body)
+			.foregroundColor(Color.white)
+			.lineLimit(2)
+			.padding()
+			.background(
+				Capsule()
+					.fill(fill)
+					.overlay(alignment: .bottom) {
+						RoundedTriangle(radius: 4)
+							.fill(fill)
+							.frame(dimension: 26)
+							.rotationEffect(.degrees(90))
+							.offset(y: 18)
+					}
+			)
+			.dropShadow()
+			.dropShadow()
 	}
 }
 
 struct CalloutView_Previews: PreviewProvider {
 	static var previews: some View {
-		CalloutView(size: CGSize(width: 200, height: 60), fill: Color.app.secondary)
+		VStack(spacing: 40) {
+			CalloutView(content: "What is my name? Could you tell me please?")
+			CalloutView(content: "😂")
+		}
 	}
 }
