289a8b7b8a4b176085bb4c2cadc9891393087e3c
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-12 14:22:38 -0800

    Add XButton

---
+++ Tribes/Utilities/Views/XButton.swift
@@ -0,0 +1,29 @@
+//
+//  XButton.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-12.
+//
+
+import SwiftUI
+
+struct XButton: View {
+	let action: () -> ()
+	var body: some View {
+		Button(action: { action() }) {
+			Image(systemName: "x.circle.fill")
+				.font(Font.app.title)
+				.foregroundColor(Color.white)
+		}
+		.buttonStyle(.plain)
+	}
+}
+
+struct XButton_Previews: PreviewProvider {
+	static var previews: some View {
+		XButton {
+			()
+		}
+		.preferredColorScheme(.dark)
+	}
+}
