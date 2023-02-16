573cbd3724219e4b8d04e3c4773f35a7d65f4370
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-15 22:11:43 -0800

    Add encryptAES and decryptAES functions in the Encryption Client

--- Tribes/Clients/EncryptionClient/EncryptionClient.swift
+++ Tribes/Clients/EncryptionClient/EncryptionClient.swift
@@ -5,6 +5,7 @@
 //  Created by Kingsley Okeke on 2023-02-15.
 //
 
+import CryptoKit
 import Foundation
 
 protocol EncryptionClientProtocol {
@@ -21,5 +22,29 @@ class EncryptionClient: EncryptionClientProtocol {
 	
 	func decrypt(_ data: Data, key: String) -> Data {
 		return Data()
+	}
+	
+	func encryptAES(message: Data, key: String) -> Data? {
+		guard let keyData = Data(base64Encoded: key) else { return nil }
+		let sealedMessage = try? AES.GCM.seal(message, using: SymmetricKey(data: keyData))
+		return sealedMessage?.combined
+	}
+	
+	func decryptAES(sealedMessage: Data, key: String) -> Data? {
+		let sealedBox = try? AES.GCM.SealedBox(combined: sealedMessage)
+		guard
+			let keyData = Data(base64Encoded: key),
+			let sealedBox = sealedBox
+		else { return nil }
+		let openedMessage = try? AES.GCM.open(sealedBox, using: SymmetricKey(data: keyData))
+		return openedMessage
+	}
+}
+
+fileprivate extension SymmetricKey {
+	func toBase64EncodedString() -> String {
+		return self.withUnsafeBytes { body in
+			Data(body).base64EncodedString()
+		}
 	}
 }
