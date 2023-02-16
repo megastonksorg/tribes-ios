f2789ad556b9430b83e4de4f38537ab7dd1b9762
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-15 20:24:00 -0800

    Add encrypt and decrypt base functions

--- Tribes/Clients/EncryptionClient/EncryptionClient.swift
+++ Tribes/Clients/EncryptionClient/EncryptionClient.swift
@@ -8,9 +8,18 @@
 import Foundation
 
 protocol EncryptionClientProtocol {
-	
+	func encrypt(_ data: Data, publicKey: String) -> Data
+	func decrypt(_ data: Data, key: String) -> Data
 }
 
 class EncryptionClient: EncryptionClientProtocol {
 	static let shared: EncryptionClient = EncryptionClient()
+	
+	func encrypt(_ data: Data, publicKey: String) -> Data {
+		return Data()
+	}
+	
+	func decrypt(_ data: Data, key: String) -> Data {
+		return Data()
+	}
 }
