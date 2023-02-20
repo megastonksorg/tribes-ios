acfe0f94da8c3f8871e9d93199b6b9786e225a45
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-20 11:17:26 -0800

    Add ChatView file and it's ViewModel

---
+++ Tribes/View/Chat/ChatVM.swift
@@ -0,0 +1,14 @@
+//
+//  ChatVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-20.
+//
+
+import Foundation
+
+extension ChatView {
+	@MainActor class ViewModel: ObservableObject {
+		
+	}
+}
---
+++ Tribes/View/Chat/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-20.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct ChatView_Previews: PreviewProvider {
+	static var previews: some View {
+		ChatView()
+	}
+}
