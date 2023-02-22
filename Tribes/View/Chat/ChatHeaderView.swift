81556914dac205ba5ec7f4d86e42c800a7c07346
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-22 00:18:59 -0800

    All the avatars for when a Tribe has 0-4 members in the ChatHeaderView

--- Tribes/View/Chat/ChatHeaderView.swift
+++ Tribes/View/Chat/ChatHeaderView.swift
@@ -5,16 +5,81 @@
 //  Created by Kingsley Okeke on 2023-02-21.
 //
 
+import IdentifiedCollections
 import SwiftUI
 
 struct ChatHeaderView: View {
+	let maxWidth: CGFloat = UIScreen.main.bounds.width
+	
+	let members: IdentifiedArrayOf<TribeMember>
 	var body: some View {
-		Text("Hello, World!")
+		HStack() {
+			let straightDimension: CGFloat = maxWidth * 0.16
+			let straightSpacing: CGFloat = maxWidth * 0.04
+			switch members.count {
+			case 0:
+				EmptyView()
+			case 1:
+				userAvatarView(members[0])
+					.frame(dimension: straightDimension)
+			case 2:
+				HStack(spacing: straightSpacing) {
+					Group {
+						userAvatarView(members[0])
+						userAvatarView(members[1])
+					}
+					.frame(dimension: straightDimension)
+				}
+			case 3:
+				HStack(spacing: straightSpacing) {
+					Group {
+						userAvatarView(members[0])
+						userAvatarView(members[1])
+						userAvatarView(members[2])
+					}
+					.frame(dimension: straightDimension)
+				}
+			case 4:
+				HStack(spacing: straightSpacing) {
+					Group {
+						userAvatarView(members[0])
+						userAvatarView(members[1])
+						userAvatarView(members[2])
+						userAvatarView(members[3])
+					}
+					.frame(dimension: straightDimension)
+				}
+			default:
+				EmptyView()
+			}
+		}
+		.frame(width: maxWidth, height: 100)
+		.background(Color.black)
+	}
+	
+	@ViewBuilder
+	func userAvatarView(_ member: TribeMember) -> some View {
+		Button(action: {}) {
+			UserAvatar(url: member.profilePhoto)
+		}
 	}
 }
 
 struct ChatHeaderView_Previews: PreviewProvider {
 	static var previews: some View {
-		ChatHeaderView()
+		VStack {
+			ChatHeaderView(
+				members: IdentifiedArrayOf(
+					uniqueElements:
+						[
+							TribeMember.noop1,
+							TribeMember.noop2,
+							TribeMember.noop3,
+							TribeMember.noop4
+						]
+				)
+			)
+			Spacer()
+		}
 	}
 }
