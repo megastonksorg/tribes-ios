1ce54d838b465e442853c473bbe54defed04a635
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-21 23:49:48 -0800

    Add ChatHeaderView file to the ChatGroup

---
+++ Tribes/View/Chat/ChatHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatHeaderView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-21.
+//
+
+import SwiftUI
+
+struct ChatHeaderView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct ChatHeaderView_Previews: PreviewProvider {
+	static var previews: some View {
+		ChatHeaderView()
+	}
+}
