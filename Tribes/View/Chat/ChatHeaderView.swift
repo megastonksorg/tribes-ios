cbfbde7e2c19271b69849818dcf3e2a7df9ab19f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-22 08:20:50 -0800

    Add ChatHeader view for 5 members

--- Tribes/View/Chat/ChatHeaderView.swift
+++ Tribes/View/Chat/ChatHeaderView.swift
@@ -14,40 +14,59 @@ struct ChatHeaderView: View {
 	let members: IdentifiedArrayOf<TribeMember>
 	var body: some View {
 		HStack() {
-			let straightDimension: CGFloat = maxWidth * 0.16
-			let straightSpacing: CGFloat = maxWidth * 0.04
+			let dimensionA: CGFloat = maxWidth * 0.16
+			let spacingA: CGFloat = maxWidth * 0.04
+			
+			let dimensionB: CGFloat = maxWidth * 0.14
+			let spacingB: CGFloat = maxWidth * 0.2
+			
 			switch members.count {
 			case 0:
 				EmptyView()
 			case 1:
 				userAvatarView(members[0])
-					.frame(dimension: straightDimension)
+					.frame(dimension: dimensionA)
 			case 2:
-				HStack(spacing: straightSpacing) {
+				HStack(spacing: spacingA) {
 					Group {
 						userAvatarView(members[0])
 						userAvatarView(members[1])
 					}
-					.frame(dimension: straightDimension)
+					.frame(dimension: dimensionA)
 				}
 			case 3:
-				HStack(spacing: straightSpacing) {
+				HStack(spacing: spacingA) {
 					Group {
 						userAvatarView(members[0])
 						userAvatarView(members[1])
 						userAvatarView(members[2])
 					}
-					.frame(dimension: straightDimension)
+					.frame(dimension: dimensionA)
 				}
 			case 4:
-				HStack(spacing: straightSpacing) {
+				HStack(spacing: spacingA) {
 					Group {
 						userAvatarView(members[0])
 						userAvatarView(members[1])
 						userAvatarView(members[2])
 						userAvatarView(members[3])
 					}
-					.frame(dimension: straightDimension)
+					.frame(dimension: dimensionA)
+				}
+			case 5:
+				VStack(spacing: 0) {
+					HStack(spacing: spacingB) {
+						userAvatarView(members[0])
+						userAvatarView(members[1])
+						userAvatarView(members[2])
+					}
+					.frame(dimension: dimensionB)
+					HStack(spacing: spacingB) {
+						userAvatarView(members[3])
+						userAvatarView(members[4])
+					}
+					.frame(dimension: dimensionB)
+					.offset(y: -dimensionB/2.4)
 				}
 			default:
 				EmptyView()
@@ -75,7 +94,8 @@ struct ChatHeaderView_Previews: PreviewProvider {
 							TribeMember.noop1,
 							TribeMember.noop2,
 							TribeMember.noop3,
-							TribeMember.noop4
+							TribeMember.noop4,
+							TribeMember.noop5,
 						]
 				)
 			)
