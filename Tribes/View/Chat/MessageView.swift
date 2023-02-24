9e2e8a56c7e8e3f260b56066d878a67ce47ac104
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-23 20:56:56 -0800

    Add MessageView file

---
+++ Tribes/View/Chat/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct MessageView_Previews: PreviewProvider {
+	static var previews: some View {
+		MessageView()
+	}
+}
