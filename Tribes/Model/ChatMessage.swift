622208f15ddae9f73e1e4417129828c80b4ae351
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-24 00:00:13 -0800

    Add timestamp to Chat Message

--- Tribes/Model/ChatMessage.swift
+++ Tribes/Model/ChatMessage.swift
@@ -24,13 +24,22 @@ class ChatMessage: Identifiable {
 	let context: ChatMessage?
 	let sender: TribeMember
 	let style: Style
+	let timeStamp: Date
 	
-	init(id: String, content: Content, context: ChatMessage? = nil, sender: TribeMember, style: Style) {
+	init(
+		id: String,
+		content: Content,
+		context: ChatMessage? = nil,
+		sender: TribeMember,
+		style: Style,
+		timeStamp: Date
+	) {
 		self.id = id
 		self.content = content
 		self.context = context
 		self.sender = sender
 		self.style = style
+		self.timeStamp = timeStamp
 	}
 }
 
@@ -39,24 +48,28 @@ extension ChatMessage {
 		id: UUID().uuidString,
 		content: .text("Stop there. Please change account info when you get a chance"),
 		sender: TribeMember.noop1,
-		style: .incoming
+		style: .incoming,
+		timeStamp: Calendar.current.date(byAdding: .month, value: -2, to: Date())!
 	)
 	static let noop2: ChatMessage = ChatMessage(
 		id: UUID().uuidString,
 		content: .text("Where are we going for dinner? 🍽️ 🍛 Are you all coming?!"),
 		sender: TribeMember.noop2,
-		style: .incoming
+		style: .incoming,
+		timeStamp: Calendar.current.date(byAdding: .minute, value: -10, to: Date())!
 	)
 	static let noop3: ChatMessage = ChatMessage(
 		id: UUID().uuidString,
 		content: .text("Hey there, are you really sure you would like to go through with this?"),
 		sender: TribeMember.noop3,
-		style: .incoming
+		style: .incoming,
+		timeStamp: Calendar.current.date(byAdding: .minute, value: -5, to: Date())!
 	)
 	static let noop4: ChatMessage = ChatMessage(
 		id: UUID().uuidString,
 		content: .text("Hahaha 😂😂 Well, well, isn't that a crazy coincidence?"),
 		sender: TribeMember.noop4,
-		style: .outgoing
+		style: .outgoing,
+		timeStamp: Date.now
 	)
 }
