37719c429f4fca1e695820449838213b982ef082
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-23 23:14:08 -0800

    Add noop objects for ChatMessage

--- Tribes/Model/ChatMessage.swift
+++ Tribes/Model/ChatMessage.swift
@@ -21,15 +21,42 @@ class ChatMessage: Identifiable {
 	
 	let id: String
 	let content: Content
-	let context: ChatMessage
+	let context: ChatMessage?
 	let sender: TribeMember
 	let style: Style
 	
-	init(id: String, content: Content, context: ChatMessage, sender: TribeMember, style: Style) {
+	init(id: String, content: Content, context: ChatMessage? = nil, sender: TribeMember, style: Style) {
 		self.id = id
 		self.content = content
 		self.context = context
 		self.sender = sender
 		self.style = style
 	}
+}
+
+extension ChatMessage {
+	static let noop1: ChatMessage = ChatMessage(
+		id: UUID().uuidString,
+		content: .text("Stop there. Please change account info when you get a chance"),
+		sender: TribeMember.noop1,
+		style: .incoming
+	)
+	static let noop2: ChatMessage = ChatMessage(
+		id: UUID().uuidString,
+		content: .text("Where are we going for dinner? 🍽️ 🍛 Are you all coming?!"),
+		sender: TribeMember.noop2,
+		style: .incoming
+	)
+	static let noop3: ChatMessage = ChatMessage(
+		id: UUID().uuidString,
+		content: .text("Hey there, are you really sure you would like to go through with this?"),
+		sender: TribeMember.noop3,
+		style: .incoming
+	)
+	static let noop4: ChatMessage = ChatMessage(
+		id: UUID().uuidString,
+		content: .text("Hahaha 😂😂 Well, well, isn't that a crazy coincidence?"),
+		sender: TribeMember.noop4,
+		style: .outgoing
+	)
 }
