1db4974d270ca2a73c9886bc42d3bb8d2f3d4c5f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-24 00:16:59 -0800

    Add text view in the ChatMessageView

--- Tribes/View/Chat/ChatMessageView.swift
+++ Tribes/View/Chat/ChatMessageView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct ChatMessageView: View {
+	let message: ChatMessage
+	
 	var body: some View {
-		Text("Hello, World!")
+		contentView()
+	}
+	
+	@ViewBuilder
+	func contentView() -> some View {
+		VStack(alignment: message.style == .incoming ? .leading :.trailing) {
+			switch message.content {
+			case .text:
+				textView(content: message.content)
+			case .imageWithCaption, .videoWithCaption:
+				EmptyView()
+			}
+		}
+	}
+	
+	@ViewBuilder
+	func textView(content: ChatMessage.Content) -> some View {
+		let corners: UIRectCorner = {
+			switch message.style {
+			case .incoming: return [.topRight, .bottomLeft, .bottomRight]
+			case .outgoing: return [.topLeft, .topRight, .bottomLeft]
+			}
+		}()
+		
+		let color: Color = {
+			switch message.style {
+			case .incoming: return Color.app.secondary
+			case .outgoing: return Color.app.tertiary
+			}
+		}()
+		
+		let foregroundColor: Color = {
+			switch message.style {
+			case .incoming: return Color.white
+			case .outgoing: return Color.black
+			}
+		}()
+		if case .text(let text) = content {
+			Text(text ?? "Message Could not decrypted")
+				.font(Font.app.subTitle)
+				.foregroundColor(foregroundColor)
+				.padding()
+				.background(
+					CustomRoundedRectangle(cornerRadius: 30, corners: corners)
+						.fill(color)
+				)
+		}
 	}
 }
 
 struct ChatMessageView_Previews: PreviewProvider {
 	static var previews: some View {
-		ChatMessageView()
+		ChatMessageView(message: ChatMessage.noop1)
 	}
 }
