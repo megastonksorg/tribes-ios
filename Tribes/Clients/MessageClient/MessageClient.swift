e01acf35808da9726030d065b3b10c415e9a37f2
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-28 00:56:30 -0800

    Add MessageClient folder and it's file

---
+++ Tribes/Clients/MessageClient/MessageClient.swift
@@ -0,0 +1,16 @@
+//
+//  MessageClient.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-02-28.
+//
+
+import Foundation
+
+protocol MessageClientProtocol {
+	
+}
+
+class MessageClient: MessageClientProtocol {
+	
+}
