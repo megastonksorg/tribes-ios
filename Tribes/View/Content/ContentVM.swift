1300c3e532b976d2e31054203067be3a6aa98e9b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-01 09:55:20 -0800

    Add ContentView and ContentVM

---
+++ Tribes/View/Content/ContentVM.swift
@@ -0,0 +1,14 @@
+//
+//  ContentVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-03-01.
+//
+
+import Foundation
+
+extension ContentView {
+	@MainActor class ViewModel: ObservableObject {
+		
+	}
+}
---
+++ Tribes/View/Content/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
