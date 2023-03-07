bcf2981b987ce735a06734cce2e4a42150eb2334
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-06 20:50:25 -0800

    Add MessageVideoView file

---
+++ Tribes/View/Message/Body/MessageVideoView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageVideoView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-03-06.
+//
+
+import SwiftUI
+
+struct MessageVideoView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct MessageVideoView_Previews: PreviewProvider {
+	static var previews: some View {
+		MessageVideoView()
+	}
+}
