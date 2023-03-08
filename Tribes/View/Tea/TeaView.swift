cbc8c0006412de54623ab52b0412bb740fb73a09
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-07 23:50:56 -0800

    Add Header view to the TeaView

--- Tribes/View/Tea/TeaView.swift
+++ Tribes/View/Tea/TeaView.swift
@@ -25,11 +25,44 @@ struct TeaView: View {
 			.frame(size: proxy.size)
 		}
 		.ignoresSafeArea()
+		.overlay {
+			VStack {
+				header()
+				Spacer()
+			}
+			.padding(.horizontal)
+		}
+	}
+	
+	@ViewBuilder
+	func header() -> some View {
+		HStack(spacing: 10) {
+			HStack(spacing: -12) {
+				ForEach(0..<viewModel.tribe.members.count, id: \.self) { index in
+					UserAvatar(url: viewModel.tribe.members[index].profilePhoto)
+						.frame(dimension: 24)
+						.zIndex(-Double(index))
+				}
+			}
+			HStack(spacing: 0) {
+				TextView("\(viewModel.tribe.name)  •   ", style: .tribeName(16))
+					.multilineTextAlignment(.leading)
+					.lineLimit(2)
+				Text("2hrs ago")
+					.font(Font.app.body)
+					.foregroundColor(Color.app.tertiary)
+			}
+			Spacer()
+			XButton {
+			}
+			.padding([.top, .leading, .bottom])
+		}
 	}
 }
 
 struct TeaView_Previews: PreviewProvider {
 	static var previews: some View {
-		TeaView(viewModel: .init(tribe: Tribe.noop1))
+		TeaView(viewModel: .init(tribe: Tribe.noop2))
+			.preferredColorScheme(.dark)
 	}
 }
