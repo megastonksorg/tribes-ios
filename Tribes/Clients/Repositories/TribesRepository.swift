52d421e823c26e71511b3594756c9882c1428ba9
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-10 10:46:10 -0800

    Refresh the messages after the tribes have been cached in the TribesRepository

--- Tribes/Clients/Repositories/TribesRepository.swift
+++ Tribes/Clients/Repositories/TribesRepository.swift
@@ -60,8 +60,8 @@ class TribesRepository: TribesRepositoryProtocol {
 							self.tribes = tribes
 						}
 						Task {
-							await MessageClient.shared.refreshMessages()
 							await self.cacheClient.setData(key: .tribes, value: tribes)
+							await MessageClient.shared.refreshMessages()
 						}
 						promise(.success(tribes))
 					}
