a917e27589a87391330ebbcfb791e28fb1181e24
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-09 23:39:35 -0800

    Load the video in the MessageVideoView

--- Tribes/View/Message/Body/MessageVideoView.swift
+++ Tribes/View/Message/Body/MessageVideoView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct MessageVideoView: View {
 	let model: MessageBodyModel
 	let isPlaying: Bool
+	
+	@State var url: URL? //The local Url of the video to play
+	@State var isLoadingVideo: Bool = true
+	
+	//Clients
+	let cacheClient: CacheClient = CacheClient.shared
+	
 	var body: some View {
 		Group {
 			if model.message.isEncrypted {
 				NoContentView(isEncrypted: true)
 			} else {
-				if case .video(let url) = model.message.body?.content {
+				if isLoadingVideo {
+					
+				} else if let url = self.url {
 					VideoPlayerView(url: url, isPlaying: isPlaying)
 						.overlay {
 							if let caption = model.message.body?.caption {
@@ -24,10 +33,46 @@ struct MessageVideoView: View {
 									.offset(y: SizeConstants.teaCaptionOffset)
 							}
 						}
+				} else {
+					NoContentView(isEncrypted: false)
 				}
 			}
 		}
 		.ignoresSafeArea()
+		.task {
+			if url == nil {
+				loadVideo()
+			}
+		}
+	}
+	
+	func loadVideo() {
+		Task {
+			guard let cacheKey = Cache.getContentCacheKey(encryptedContent: model.message.encryptedBody.content)
+			else {
+				self.isLoadingVideo = false
+				return
+			}
+			
+			let directory = FileManager.default.temporaryDirectory
+			let fileName = "\(cacheKey)\(AppConstants.videoFileType)"
+			let url = directory.appendingPathComponent(fileName)
+			if FileManager.default.fileExists(atPath: url.path()) {
+				self.url = url
+				self.isLoadingVideo = false
+				return
+			} else {
+				//Load Video Data from Cache if it does not exist in the temp directory
+				guard let videoData = await cacheClient.get(key: cacheKey, type: Data.self)
+				else {
+					self.isLoadingVideo = false
+					return
+				}
+				try? videoData.write(to: url)
+				self.url = url
+				self.isLoadingVideo = false
+			}
+		}
 	}
 }
 
