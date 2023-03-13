032b4b0e4227ace99e979380db25036a3783985b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-13 15:02:20 -0700

    Rename Push notification files then add sounds enum to the SoundsClient

--- Tribes/Clients/SoundClient.swift
+++ Tribes/Clients/SoundClient.swift
@@ -9,12 +9,18 @@ import AVKit
 import Foundation
 
 class SoundClient {
+	enum Sound: String {
+		case inAppNotification
+		case pushNotification
+		case sendMessage
+	}
+	
 	static let shared: SoundClient = SoundClient()
 	
 	var player: AVAudioPlayer? = nil
 	
-	func playSound() {
-		guard let url = Bundle.main.url(forResource: "wave", withExtension: ".mp3") else { return }
+	func playSound(_ sound: Sound) {
+		guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
 		self.player = try? AVAudioPlayer(contentsOf: url)
 		self.player?.play()
 	}
