0f78512c0cdab6049e1a82efb18a3699bcaf10c0
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-15 05:23:52 -0700

    Increase the font size for smaller Tribes Avatars

--- Tribes/Utilities/Extensions/CGFloat+Extensions.swift
+++ Tribes/Utilities/Extensions/CGFloat+Extensions.swift
@@ -10,8 +10,8 @@ import Foundation
 extension CGFloat {
 	func getTribeNameSize() -> Self {
 		switch self {
-		case 0..<100: return 12
-		case 100..<250: return 15
+		case 0..<100: return 14
+		case 100..<250: return 16
 		case 250..<400: return 18
 		default: return 22
 		}
