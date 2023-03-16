37b43e6b861c62b57f805d81b35d827f5e35e5fd
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-16 09:28:14 -0700

    Start the SignalR connection in the Hub and register for the receiveMessage event

--- Tribes/Clients/HubClient/HubClient.swift
+++ Tribes/Clients/HubClient/HubClient.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import SignalRClient
 
 class HubClient {
+	private var connection: HubConnection
 	
+	init() {
+		connection = HubConnectionBuilder(url: URL(string: "https://\(APPUrlRequest.domain)/appHub")!)
+			.withLogging(minLogLevel: .error)
+			.withAutoReconnect()
+			.build()
+		
+		//Register for Events
+		connection.on(method: "receiveMessage", callback: { (tribeId: String, message: MessageResponse) in
+			self.handleMessage(tribeId, message: message)
+		})
+		
+		connection.start()
+	}
+	
+	private func handleMessage(_ tribeId: String, message: MessageResponse) {
+		Task {
+			await MessageClient.shared.messageReceived(tribeId: tribeId, messageResponse: message)
+		}
+	}
 }
