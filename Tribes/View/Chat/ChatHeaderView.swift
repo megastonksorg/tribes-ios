bf6d0fba1cfa943bdfb70329cc29b8d1a40f8395
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-04-27 12:27:47 -0700

    Update the ChatHeaderView

--- Tribes/View/Chat/ChatHeaderView.swift
+++ Tribes/View/Chat/ChatHeaderView.swift
@@ -9,200 +9,53 @@ import IdentifiedCollections
 import SwiftUI
 
 struct ChatHeaderView: View {
-	let maxWidth: CGFloat = UIScreen.main.bounds.width
-	
-	let action: (TribeMember) -> Void
 	let members: IdentifiedArrayOf<TribeMember>
 	
 	var body: some View {
-		HStack() {
-			let dimensionA: CGFloat = maxWidth * 0.14
-			let spacingA: CGFloat = maxWidth * 0.08
-			
-			let dimensionB: CGFloat = maxWidth * 0.11
-			let spacingB: CGFloat = maxWidth * 0.20
-			
-			switch members.count {
-			case 1:
-				userAvatarView(members[0])
-					.frame(dimension: dimensionA)
-			case 2:
-				HStack(spacing: spacingA) {
-					Group {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-					}
-					.frame(dimension: dimensionA)
-				}
-			case 3:
-				HStack(spacing: spacingA) {
-					Group {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-					}
-					.frame(dimension: dimensionA)
-				}
-			case 4:
-				HStack(spacing: spacingA) {
-					Group {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-						userAvatarView(members[3])
-					}
-					.frame(dimension: dimensionA)
-				}
-			case 5:
-				VStack(spacing: 0) {
-					HStack(spacing: spacingB) {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-					}
-					.frame(dimension: dimensionB)
-					HStack(spacing: spacingB) {
-						userAvatarView(members[3])
-						userAvatarView(members[4])
-					}
-					.frame(dimension: dimensionB)
-					.offset(y: -dimensionB * 0.3)
-				}
-			case 6:
-				let spacing: CGFloat = maxWidth * 0.15
-				VStack(spacing: 0) {
-					HStack(spacing: spacing) {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-					}
-					.frame(dimension: dimensionB)
-					.offset(x: -dimensionB * 0.4)
-					HStack(spacing: spacing) {
-						userAvatarView(members[3])
-						userAvatarView(members[4])
-						userAvatarView(members[5])
-					}
-					.frame(dimension: dimensionB)
-					.offset(x: dimensionB * 0.4, y: -dimensionB * 0.2)
-				}
-			case 7:
-				let dimension: CGFloat = maxWidth * 0.10
-				let spacing: CGFloat = maxWidth * 0.15
-				VStack(spacing: 0) {
-					HStack(spacing: spacing) {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-						userAvatarView(members[3])
-					}
+		let limit: Int = 10
+		let spacing: CGFloat = 6
+		let dimension: CGFloat = 36
+		HStack(spacing: -spacing) {
+			ForEach(members.prefix(limit)) {
+				UserAvatar(url: $0.profilePhoto)
 					.frame(dimension: dimension)
-					HStack(spacing: spacing) {
-						userAvatarView(members[4])
-						userAvatarView(members[5])
-						userAvatarView(members[6])
-					}
-					.frame(dimension: dimension)
-					.offset(y: -dimension * 0.15)
-				}
-			case 8:
-				let dimension: CGFloat = maxWidth * 0.10
-				let spacing: CGFloat = maxWidth * 0.10
-				VStack(spacing: 0) {
-					HStack(spacing: spacing) {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-						userAvatarView(members[3])
-					}
-					.frame(dimension: dimension)
-					.offset(x: -dimension * 0.4)
-					HStack(spacing: spacing) {
-						userAvatarView(members[4])
-						userAvatarView(members[5])
-						userAvatarView(members[6])
-						userAvatarView(members[7])
-					}
-					.frame(dimension: dimension)
-					.offset(x: dimension * 0.4, y: -dimension * 0.2)
-				}
-			case 9:
-				let dimension: CGFloat = maxWidth * 0.10
-				let spacing: CGFloat = maxWidth * 0.08
-				VStack(spacing: 6) {
-					HStack(spacing: spacing) {
-						userAvatarView(members[0])
-						userAvatarView(members[1])
-						userAvatarView(members[2])
-						userAvatarView(members[3])
-						userAvatarView(members[4])
-					}
-					.frame(dimension: dimension)
-					HStack(spacing: spacing) {
-						userAvatarView(members[5])
-						userAvatarView(members[6])
-						userAvatarView(members[7])
-						userAvatarView(members[8])
-					}
-					.frame(dimension: dimension)
-					.offset(y: -dimension * 0.15)
-				}
-			default:
-				ScrollView(.horizontal) {
-					LazyHGrid(rows: [GridItem(), GridItem()], spacing: 20) {
-						ForEach(members) {
-							userAvatarView($0)
-						}
-					}
-				}
-				.padding(.horizontal)
-				.padding(.bottom, 10)
 			}
-		}
-		.padding(.bottom, 8)
-		.frame(width: maxWidth, height: 85)
-		.background {
-			let cornerRadius: CGFloat = 40
-			let corners: UIRectCorner = [.bottomLeft, .bottomRight]
-			ZStack {
-				CustomRoundedRectangle(cornerRadius: cornerRadius, corners: corners)
-					.fill(.ultraThinMaterial)
-				CustomRoundedRectangle(cornerRadius: cornerRadius, corners: corners)
-					.fill(Color.app.background.opacity(0.6))
+			if members.count > limit {
+				Circle()
+					.fill(Color.app.secondary)
+					.frame(dimension: dimension)
+					.overlay(
+						Text("+\(members.count - limit)")
+							.font(Font.app.title3)
+							.foregroundColor(Color.white)
+					)
 			}
-			.edgesIgnoringSafeArea(.top)
-		}
-	}
-	
-	@ViewBuilder
-	func userAvatarView(_ member: TribeMember) -> some View {
-		Button(action: { action(member) }) {
-			UserAvatar(url: member.profilePhoto)
 		}
 	}
 }
 
 struct ChatHeaderView_Previews: PreviewProvider {
 	static var previews: some View {
-		VStack {
-			ChatHeaderView(
-				action: { _ in },
-				members: IdentifiedArrayOf(
-					uniqueElements:
-						[
-							TribeMember.noop1,
-							TribeMember.noop2,
-							TribeMember.noop3,
-							TribeMember.noop4,
-							TribeMember.noop5,
-							TribeMember.noop6,
-							TribeMember.noop7,
-							TribeMember.noop8,
-							TribeMember.noop9
-						]
+		NavigationView {
+			VStack {
+				ChatHeaderView(
+					members: IdentifiedArrayOf(
+						uniqueElements:
+							[
+								TribeMember.noop1,
+								TribeMember.noop2,
+								TribeMember.noop3,
+								TribeMember.noop4,
+								TribeMember.noop5,
+								TribeMember.noop6,
+								TribeMember.noop7,
+								TribeMember.noop8,
+								TribeMember.noop9
+							]
+					)
 				)
-			)
-			Spacer()
+				Spacer()
+			}
 		}
 	}
 }
