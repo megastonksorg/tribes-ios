a192c6a0074f44d4dc5f12b79f749f6289b09caa
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-07 11:40:36 -0700

    Add the TribeAvatar to the TribeProfile view

--- Tribes/View/TribeProfile/TribeProfileVM.swift
+++ Tribes/View/TribeProfile/TribeProfileVM.swift
@@ -5,4 +5,24 @@
 //  Created by Kingsley Okeke on 2023-05-07.
 //
 
+import Combine
 import Foundation
+
+extension TribeProfileView {
+	@MainActor class ViewModel: ObservableObject {
+		enum Stack: Hashable {
+			case userProfile
+		}
+		
+		let tribe: Tribe
+		
+		private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+		
+		@Published var stack: [Stack] = []
+		@Published var banner: BannerData?
+		
+		init(tribe: Tribe) {
+			self.tribe = tribe
+		}
+	}
+}
--- Tribes/View/TribeProfile/TribeProfileView.swift
+++ Tribes/View/TribeProfile/TribeProfileView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct TribeProfileView: View {
+	@StateObject var viewModel: ViewModel
+	
 	var body: some View {
-		Text("Hello, World!")
+		NavigationStack(path: $viewModel.stack) {
+			VStack {
+				HStack {
+					Spacer()
+					Button(action: {}) {
+						Text("Cancel")
+							.font(Font.app.title2)
+							.foregroundColor(Color.white)
+					}
+				}
+				.padding(.horizontal)
+				
+				TribeAvatar(
+					context: .profileView,
+					tribe: viewModel.tribe,
+					size: 200,
+					primaryAction: { _ in },
+					secondaryAction: { _ in }
+				)
+				.disabled(true)
+				Spacer()
+			}
+			.pushOutFrame()
+			.background(Color.app.background)
+		}
 	}
 }
 
 struct TribeProfile_Previews: PreviewProvider {
 	static var previews: some View {
-		TribeProfileView()
+		TribeProfileView(viewModel: .init(tribe: Tribe.noop2))
 	}
 }
