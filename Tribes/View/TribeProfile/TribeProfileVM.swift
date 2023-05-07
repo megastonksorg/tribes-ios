c83f34174526da5ddfe8291ab3678d0c3d95a271
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-07 11:24:12 -0700

    Add TribeProfileView file and the TribeProfileVM

---
+++ Tribes/View/TribeProfile/TribeProfileVM.swift
@@ -0,0 +1,8 @@
+//
+//  TribeProfileVM.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-05-07.
+//
+
+import Foundation
---
+++ Tribes/View/TribeProfile/TribeProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  TribeProfileView.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-05-07.
+//
+
+import SwiftUI
+
+struct TribeProfileView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct TribeProfile_Previews: PreviewProvider {
+	static var previews: some View {
+		TribeProfileView()
+	}
+}
