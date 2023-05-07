7a0b2498a3698c2a422393b81dd6eabd854fd71b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-07 14:44:29 -0700

    Clean up spacing in the TribeProfileView and layout

--- Tribes/View/TribeProfile/TribeProfileView.swift
+++ Tribes/View/TribeProfile/TribeProfileView.swift
@@ -23,83 +23,78 @@ struct TribeProfileView: View {
 							.foregroundColor(Color.white)
 					}
 				}
-				.padding(.horizontal)
-				ScrollView {
-					VStack {
-						let tribeAvatarSize: CGFloat = 200
-						let padding: CGFloat = 20
+				.padding([.top, .trailing])
+				VStack(spacing: 2) {
+					let tribeAvatarSize: CGFloat = 200
+					
+					TribeAvatar(
+						context: .profileView,
+						tribe: viewModel.tribe,
+						size: tribeAvatarSize,
+						primaryAction: { _ in },
+						secondaryAction: { _ in }
+					)
+					.disabled(true)
+					
+					ZStack {
+						let fontSize: CGFloat =  tribeAvatarSize.getTribeNameSize() + 4
 						
-						TribeAvatar(
-							context: .profileView,
-							tribe: viewModel.tribe,
-							size: tribeAvatarSize,
-							primaryAction: { _ in },
-							secondaryAction: { _ in }
-						)
-						.disabled(true)
-						
-						ZStack {
-							let fontSize: CGFloat =  tribeAvatarSize.getTribeNameSize() + 4
-							
-							TribeNameView(name: viewModel.tribe.name, shouldShowEditIcon: true, fontSize: fontSize) {
-								viewModel.editTribeName()
-							}
-							.opacity(viewModel.isEditingTribeName ? 0.0 : 1.0)
-							
-							if viewModel.isEditingTribeName {
-								TextField("", text: $viewModel.editTribeNameText)
-									.disableAutoCorrection(isNameField: false)
-									.submitLabel(.done)
-									.onSubmit { viewModel.updateTribeName() }
-									.font(.system(size: fontSize, weight: .medium, design: .rounded))
-									.foregroundColor(Color.app.tertiary)
-									.multilineTextAlignment(.center)
-									.focused($focusedField, equals: .editTribeName)
-									.onAppear { self.focusedField = .editTribeName }
-									.onAppear { self.focusedField = nil }
-							}
+						TribeNameView(name: viewModel.tribe.name, shouldShowEditIcon: true, fontSize: fontSize) {
+							viewModel.editTribeName()
 						}
-						.padding(.horizontal, 20)
-						
-						HStack(spacing: 10) {
-							Spacer()
-							actionButton(action: {}) {
-								VStack {
-									Image(systemName: "person.fill.badge.plus")
-										.font(.system(size: FontSizes.title3))
-									Text("Invite")
-								}
-							}
-							.disabled(viewModel.tribe.members.count >= 10)
-							Spacer()
-							actionButton(action: {}) {
-								VStack {
-									Image(systemName: "rectangle.portrait.and.arrow.forward.fill")
-										.font(.system(size: FontSizes.body))
-									Text("Leave")
-								}
-							}
-							Spacer()
-						}
-						.font(.system(size: FontSizes.body))
-						.foregroundColor(Color.white)
-						.padding(.horizontal, padding)
-						.padding(.vertical, padding / 1.5)
-						.padding(.horizontal)
-						.transition(.asymmetric(insertion: .scale, removal: .identity))
 						.opacity(viewModel.isEditingTribeName ? 0.0 : 1.0)
-						.animation(.easeInOut, value: viewModel.isEditingTribeName)
 						
-						VStack {
-							ForEach(viewModel.tribe.members.others) {
-								memberRow(member: $0)
+						if viewModel.isEditingTribeName {
+							TextField("", text: $viewModel.editTribeNameText)
+								.disableAutoCorrection(isNameField: false)
+								.submitLabel(.done)
+								.onSubmit { viewModel.updateTribeName() }
+								.font(.system(size: fontSize, weight: .medium, design: .rounded))
+								.foregroundColor(Color.app.tertiary)
+								.multilineTextAlignment(.center)
+								.focused($focusedField, equals: .editTribeName)
+								.onAppear { self.focusedField = .editTribeName }
+								.onAppear { self.focusedField = nil }
+						}
+					}
+					.padding(.horizontal, 20)
+					
+					HStack(spacing: 10) {
+						Spacer()
+						actionButton(action: {}) {
+							VStack {
+								Image(systemName: "person.fill.badge.plus")
+									.font(.system(size: FontSizes.title3))
+								Text("Invite")
 							}
 						}
-						
+						.disabled(viewModel.tribe.members.count >= 10)
+						Spacer()
+						actionButton(action: {}) {
+							VStack {
+								Image(systemName: "rectangle.portrait.and.arrow.forward.fill")
+									.font(.system(size: FontSizes.body))
+								Text("Leave")
+							}
+						}
 						Spacer()
 					}
+					.font(.system(size: FontSizes.callout))
+					.foregroundColor(Color.white)
+					.padding(.top)
+					.transition(.asymmetric(insertion: .scale, removal: .identity))
+					.opacity(viewModel.isEditingTribeName ? 0.0 : 1.0)
+					.animation(.easeInOut, value: viewModel.isEditingTribeName)
 				}
-				.padding(.leading)
+				ScrollView(showsIndicators: true) {
+					VStack {
+						ForEach(viewModel.tribe.members.others) {
+							memberRow(member: $0)
+						}
+					}
+				}
+				.padding([.leading, .top])
+				Spacer()
 			}
 			.pushOutFrame()
 			.background(Color.app.background)
@@ -113,7 +108,7 @@ struct TribeProfileView: View {
 	func actionButton<Label: View>(action: @escaping () -> (), label: @escaping () -> Label) -> some View {
 		Button(action: { action() }) {
 			label()
-				.frame(width: 100, height: 60)
+				.frame(width: 80, height: 50)
 				.background(Color.app.secondary, in: Capsule())
 		}
 	}
