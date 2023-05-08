f2c5308d0d3a9ff6cfc8bcf0ca869f3c3c783a3f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-07 23:26:34 -0700

    Update the TribeMemberView and the TribeMemberVM

--- Tribes/View/TribeMember/TribeMemberVM.swift
+++ Tribes/View/TribeMember/TribeMemberVM.swift
@@ -5,10 +5,73 @@
 //  Created by Kingsley Okeke on 2023-05-07.
 //
 
+import Combine
 import Foundation
 
 extension TribeMemberView {
 	@MainActor class ViewModel: ObservableObject {
+		let member: TribeMember
+		let tribe: Tribe
 		
+		private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+		
+		@Published var isProcessingRequest: Bool = false
+		@Published var banner: BannerData?
+		
+		//Clients
+		let apiClient: APIClient = APIClient.shared
+		
+		init(member: TribeMember, tribe: Tribe) {
+			self.member = member
+			self.tribe = tribe
+		}
+		
+		func requestToBlockTribeMember() {
+			
+		}
+		
+		func requestToRemoveTribeMember() {
+		
+		}
+		
+		func blockTribeMember() {
+			self.apiClient.blockMember(tribeID: tribe.id, memberId: member.id)
+				.receive(on: DispatchQueue.main)
+				.sink(
+					receiveCompletion: { [weak self] completion in
+						switch completion {
+						case .finished:
+							self?.isProcessingRequest = false
+						case .failure(let error):
+							self?.isProcessingRequest = false
+							self?.banner = BannerData(error: error)
+						}
+					},
+					receiveValue: { [weak self] _ in
+						guard let self = self else { return }
+					}
+				)
+				.store(in: &cancellables)
+		}
+		
+		func removeTribeMember() {
+			self.apiClient.removeMember(tribeID: tribe.id, memberId: member.id)
+				.receive(on: DispatchQueue.main)
+				.sink(
+					receiveCompletion: { [weak self] completion in
+						switch completion {
+						case .finished:
+							self?.isProcessingRequest = false
+						case .failure(let error):
+							self?.isProcessingRequest = false
+							self?.banner = BannerData(error: error)
+						}
+					},
+					receiveValue: { [weak self] _ in
+						guard let self = self else { return }
+					}
+				)
+				.store(in: &cancellables)
+		}
 	}
 }
--- Tribes/View/TribeMember/TribeMemberView.swift
+++ Tribes/View/TribeMember/TribeMemberView.swift
@@ -15,12 +15,48 @@ struct TribeMemberView: View {
 	}
 	
 	var body: some View {
-		Text("Hello, World!")
+		VStack(spacing: 20) {
+			let member: TribeMember = viewModel.member
+			UserAvatar(url: member.profilePhoto)
+				.frame(dimension: 200)
+				.padding(.top)
+				
+			Text(member.fullName)
+				.font(Font.app.title3)
+				.foregroundColor(Color.white)
+			if let tenure = member.joined.utcToCurrent().date?.timeAgoDisplay() {
+				Text("Joined \(tenure)")
+					.font(Font.app.footnote)
+					.foregroundColor(Color.gray)
+					.padding(.bottom)
+			}
+			Button(action: { viewModel.requestToRemoveTribeMember() }) {
+				VStack {
+					Image(systemName: "person.fill.badge.minus")
+						.font(.system(size: FontSizes.title3))
+					Text("Remove")
+				}
+				.frame(width: 100, height: 50)
+				.background(Color.app.secondary, in: Capsule())
+			}
+			Button(action: { viewModel.requestToBlockTribeMember() }) {
+				Text("Block")
+					.font(Font.app.body)
+					.textCase(.uppercase)
+					.foregroundColor(Color.gray)
+			}
+			.padding(.top)
+			Spacer()
+		}
+		.foregroundColor(Color.white)
+		.multilineTextAlignment(.center)
+		.pushOutFrame()
+		.background(Color.app.background)
 	}
 }
 
 struct TribeMemberView_Previews: PreviewProvider {
 	static var previews: some View {
-		TribeMemberView(viewModel: .init())
+		TribeMemberView(viewModel: .init(member: TribeMember.noop2, tribe: Tribe.noop2))
 	}
 }
