3ff7c49a82bb6fd16981ba35b82c0baa8f4146f6
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-09 19:23:27 -0700

    Load the message from the server and process it when a push notification is received

--- Tribes/AppDelegate.swift
+++ Tribes/AppDelegate.swift
@@ -5,9 +5,12 @@
 //  Created by Kingsley Okeke on 2023-03-19.
 //
 
+import Combine
 import UIKit
 
 final class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+	
+	private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
 	
 	override init() {
 		super.init()
@@ -20,6 +23,35 @@ final class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCent
 	) {
 		let token: String = deviceToken.hexString
 		APIClient.shared.updateDeviceToken(token)
+	}
+	
+	func application(
+		_ application: UIApplication,
+		didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+		fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+	) {
+		guard let deepLink = DeepLink(userInfo: userInfo) else {
+			completionHandler(.failed)
+			return
+		}
+		APIClient
+			.shared
+			.getMessage(messageId: deepLink.messageId)
+			.receive(on: DispatchQueue.main)
+			.sink(receiveCompletion: { completion in
+				switch completion {
+				case .finished: return
+				case .failure: return
+					completionHandler(.failed)
+				}
+			}, receiveValue: { messageResponse in
+				MessageClient.shared.processMessageResponse(tribeId: deepLink.tribeId, messageResponse: messageResponse, wasReceived: false)
+				Task {
+					try await Task.sleep(for: .seconds(4.0))
+					completionHandler(.newData)
+				}
+			})
+			.store(in: &self.cancellables)
 	}
 	
 	func userNotificationCenter(
