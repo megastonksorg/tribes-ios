ead52ca8ef983d1b05b05ba61af4ed8ee3bf4930
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-13 15:30:21 -0700

    Add MessageDraftClient file to the project

---
+++ Tribes/Clients/MessageClient/MessageDraftClient.swift
@@ -0,0 +1,13 @@
+//
+//  MessageDraftClient.swift
+//  Tribes
+//
+//  Created by Kingsley Okeke on 2023-05-13.
+//
+
+import Combine
+import Foundation
+
+@MainActor class MessageDraftClient: ObservableObject {
+	
+}
