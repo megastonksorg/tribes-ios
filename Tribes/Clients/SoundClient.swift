c10a2af7e45b2c06cbd66ff76a0322246f03b9ae
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-18 21:41:05 -0700

    Add the ability to set the audio category in the SoundClient

--- Tribes/Clients/SoundClient.swift
+++ Tribes/Clients/SoundClient.swift
@@ -8,20 +8,62 @@
 import AVKit
 import Foundation
 
-class SoundClient {
+@MainActor class SoundClient {
+	enum Category: String {
+		case record
+		case playback
+	}
+	
 	enum Sound: String {
 		case inAppNotification
 		case pushNotification
 		case messageSent
 	}
 	
-	static let shared: SoundClient = SoundClient()
+	static private (set) var shared: SoundClient = SoundClient()
 	
 	private var player: AVAudioPlayer? = nil
+	private var category: Category
+	
+	init() {
+		self.category = .playback
+		self.setAudioCategory(for: self.category)
+	}
 	
 	func playSound(_ sound: Sound) {
 		guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
 		self.player = try? AVAudioPlayer(contentsOf: url)
 		self.player?.play()
+	}
+	
+	func setAudioCategory(for category: Category) {
+		do {
+			switch category {
+			case .record:
+				if self.category == .playback {
+					try AVAudioSession.sharedInstance().setActive(false, options: [.notifyOthersOnDeactivation])
+					try AVAudioSession.sharedInstance()
+						.setCategory(
+							.playAndRecord,
+							options: [
+								.mixWithOthers,
+								.allowBluetoothA2DP,
+								.defaultToSpeaker
+							]
+					)
+					try AVAudioSession.sharedInstance().setActive(true)
+					self.category = .record
+				}
+			case .playback:
+				if self.category == .record {
+					try AVAudioSession.sharedInstance().setActive(false, options: [.notifyOthersOnDeactivation])
+					try AVAudioSession.sharedInstance().setCategory(.ambient)
+					try AVAudioSession.sharedInstance().setActive(true)
+					self.category = .playback
+				}
+			}
+		} catch {
+			print("Could not set Audio Category {\(category.rawValue)} \(error)")
+		}
 	}
 }
