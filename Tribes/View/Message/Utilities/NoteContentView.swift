29067a7bd9b8c7e120ea9f033680fd9003828573
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-05-30 11:53:34 -0700

    Add content to the NoteContentView

--- Tribes/View/Message/Utilities/NoteContentView.swift
+++ Tribes/View/Message/Utilities/NoteContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct NoteContentView: View {
+	let style: NoteBackgroundView.Style
+	let content: String
+	
+	@State private var size: CGSize = .zero
+	
 	var body: some View {
-		NoteBackgroundView(style: .orange)
+		NoteBackgroundView(style: style)
+			.readSize { self.size = $0 }
 			.ignoresSafeArea()
+			.overlay(
+				Text(content)
+					.font(.system(size: size.height * 0.04, weight: .bold, design: .rounded))
+					.foregroundColor(Color.white)
+					.multilineTextAlignment(.center)
+					.padding(.horizontal)
+			)
 	}
 }
 
 struct NoteContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		NoteContentView()
+		NoteContentView(
+			style: .green,
+			content: "Hey there, I was wondering if it is okay for me to tell you exactly how I feel. I am sorry if I screwed up. Please forgive me. There is no way you mean that!"
+		)
 	}
 }
